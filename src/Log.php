c2086aab805ccc4f88cf9ad097d3070c8e4a8519
Mat Fish <matanya@daneltech.com>
2023-03-29 16:19:46 +0300

    match logger interface

--- src/Log.php
+++ src/Log.php
@@ -2,36 +2,41 @@
 
 namespace Securetrading\Stpp\JsonInterface;
 
-class Log extends \Psr\Log\AbstractLogger {
-  protected $_logger;
+class Log extends \Psr\Log\AbstractLogger
+{
+    protected $_logger;
 
-  protected $_requestReference;
+    protected $_requestReference;
 
-  public function setLogger(\Psr\Log\LoggerInterface $logger) {
-    $this->_logger = $logger;
-    return $this;
-  }
-
-  protected function _getLogger() {
-    if ($this->_logger === null) {
-      throw new LogException('The logger has not been set.', LogException::CODE_LOGGER_NOT_SET);
+    public function setLogger(\Psr\Log\LoggerInterface $logger)
+    {
+        $this->_logger = $logger;
+        return $this;
     }
-    return $this->_logger;
-  }
 
-  public function setRequestReference($requestReference) {
-    $this->_requestReference = $requestReference;
-    return $this;
-  }
+    protected function _getLogger()
+    {
+        if ($this->_logger === null) {
+            throw new LogException('The logger has not been set.', LogException::CODE_LOGGER_NOT_SET);
+        }
+        return $this->_logger;
+    }
 
-  public function log($logLevel, $message, array $context = array()) {
-    $message = $this->_formatMessage($message);
-    $this->_getLogger()->log($logLevel, $message, $context);
-    return $this;
-  }
+    public function setRequestReference($requestReference)
+    {
+        $this->_requestReference = $requestReference;
+        return $this;
+    }
 
-  protected function _formatMessage($message) {
-    $requestReference = $this->_requestReference ? $this->_requestReference : '   NOREF';
-    return sprintf('%s - %s', $requestReference, $message);
-  }
+    public function log($logLevel, Stringable|string $message, array $context = []): void
+    {
+        $message = $this->_formatMessage($message);
+        $this->_getLogger()->log($logLevel, $message, $context);
+    }
+
+    protected function _formatMessage($message)
+    {
+        $requestReference = $this->_requestReference ? $this->_requestReference : '   NOREF';
+        return sprintf('%s - %s', $requestReference, $message);
+    }
 }
